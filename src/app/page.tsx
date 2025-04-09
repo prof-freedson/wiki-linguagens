ff99266b80c448b3cd51c37b9a08554cd53a86d9
AmandaLoureiro28 <amandaloureiro1010@gmail.com>
2025-04-09 17:14:14 -0300

    pagina da linguagem Phyton adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import LanguageList from "./components/LanguageList";
 export default function Home() {
   const languages = [
     { name: "Dart", route: "pages/dart" },
+    { name: "Phyton", route: "pages/phyton" },
     // Adicione mais linguagens aqui no futuro
   ];
 
