6e2a3903b45e957e96ec162d93958e9f28fc33ff
YoSaddi <sallihsaddi@gmail.com>
2025-04-14 15:53:29 -0300

    Página sobre C# adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   const languages = [
     { name: "Dart", route: "pages/dart" },
     // Adicione mais linguagens aqui no futuro
+    { name: "C#", route: "pages/csharp" },
   ];
 
   return (
